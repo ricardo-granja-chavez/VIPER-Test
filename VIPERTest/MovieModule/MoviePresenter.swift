ab5c901ee759321f51d1b250b885866fab43f540
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2022-04-19 12:41:05 -0500

    set movie presenter

---
+++ VIPERTest/MovieModule/MoviePresenter.swift
@@ -0,0 +1,33 @@
+//
+//  MoviePresenter.swift
+//  VIPERTest
+//
+//  Created by Ricardo Granja Chávez on 19/04/22.
+//
+
+import Foundation
+
+class MoviePresenter:ViewToPresenterMovieProtocol{
+    
+    var view: PresenterToViewMovieProtocol?
+    var interactor: PresenterToInteractorMovieProtocol?
+    var router: PresenterToRouterMovieProtocol?
+    
+    func startFetchingMovie() {
+        interactor?.fetchMovie()
+    }
+    
+}
+
+extension MoviePresenter:InteractorToPresenterMovieProtocol{
+    
+    func movieFetchSuccess(movieList: Array<MovieModel>) {
+        view?.onMovieResponseSuccess(movieModelArrayList: movieList)
+    }
+    
+    func movieFetchFailed() {
+        view?.onMovieResponseFailed(error: "Some Error message from api response")
+    }
+    
+    
+}
