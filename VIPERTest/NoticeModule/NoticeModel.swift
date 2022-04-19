9f54a6e96dea416e70569a61b2caefabd9ddee88
Ricardo Granja Chávez <ricardo.granja.chavez@gmail.com>
2022-04-19 09:45:30 -0500

    set entitys

---
+++ VIPERTest/NoticeModule/NoticeModel.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeModel.swift
+//  VIPERTest
+//
+//  Created by Ricardo Granja Chávez on 18/04/22.
+//
+
+import Foundation
+
+class NoticeModel: Codable {
+    
+    let id: String?
+    let title: String?
+    let brief: String?
+    let filesource: String?
+    
+}
